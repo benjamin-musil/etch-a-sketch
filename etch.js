1239ad20094ac5e9051a9e215c1e317c0418b410
benjamin-musil <benjamin.musil@gmail.com>
2020-03-28 11:34:36 -0500

    change row naming to column

--- etch.js
+++ etch.js
@@ -4,7 +4,7 @@ let squareSide = 16;
 
 for (let i = 0; i < squareSide; i++) {
 	const row = document.createElement('div');
-	row.classList.add('row');
+	row.classList.add('column');
 
 	for (let j = 0; j < squareSide; j++) {
 		const square = document.createElement('div');
